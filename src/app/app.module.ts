572152fa13e6b94d3dede082eaf7a61c4c10d087
thanhle030402 <lethithanh030402@gmail.com>
2023-11-07 16:22:56 +0700

    thanh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { SidebarComponent } from './component/sidebar/sidebar.component';
+import { ContentComponent } from './component/content/content.component';
+import { HomepageComponent } from './component/homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    SidebarComponent,
+    ContentComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
